672c4def4e486d4ddf74375d10764ff03e4e5c14
Rituraj Borpujari <mail.riturajborpujari@gmail.com>
2024-08-22 13:30:33 +0530

    fixes and minor improvements
    - journal file is created with only read permission for the user
    - if env JOURNAL_DATA_FILE is not set, `.journal` is taken as the file
      path
    - gofmt formatting

--- main.go
+++ main.go
@@ -12,28 +12,27 @@ import (
 )
 
 func main() {
-  var newPost = flag.String("p", "", "Post content")
-  flag.Parse()
+	var newPost = flag.String("p", "", "Post content")
+	flag.Parse()
 
 	journalFile, ok := os.LookupEnv("JOURNAL_DATA_FILE")
 	if !ok {
-		fmt.Printf("error: could not find JOURNAL_DATA_FILE in env")
-		os.Exit(1)
+		journalFile = ".journal"
 	}
 
-  if flag.NFlag() > 0 {
-    appendPost(journalFile, *newPost)
-    os.Exit(0)
-  }
+	if flag.NFlag() > 0 {
+		appendPost(journalFile, *newPost)
+		os.Exit(0)
+	}
 
-  searchPattern := buildSearchPattern()
-  displayJournal(journalFile, searchPattern)
+	searchPattern := buildSearchPattern()
+	displayJournal(journalFile, searchPattern)
 }
 
 func displayJournal(journalFile string, searchPattern *regexp.Regexp) {
-  file, err := os.OpenFile(journalFile, os.O_RDONLY, fs.FileMode(os.O_SYNC)) 
+	file, err := os.OpenFile(journalFile, os.O_RDONLY, fs.FileMode(os.O_SYNC))
 	if err != nil {
-		fmt.Printf("error: could not read file %s", journalFile)
+		fmt.Printf("error: could not read file %s\n", journalFile)
 		os.Exit(2)
 	}
 	scanner := bufio.NewScanner(file)
@@ -56,28 +55,27 @@ func buildSearchPattern() *regexp.Regexp {
 
 	searchPattern, err := regexp.Compile(combinedArgs)
 	if err != nil {
-		fmt.Printf("error: could not build search expression from '%s'", combinedArgs)
+		fmt.Printf("error: could not build search expression from '%s'\n", combinedArgs)
 		os.Exit(3)
 	}
 
-  return searchPattern
+	return searchPattern
 }
 
 func appendPost(journalFile string, post string) {
-    file, err := os.OpenFile(journalFile, os.O_APPEND | os.O_WRONLY | os.O_CREATE, fs.FileMode(os.O_RDWR))
-    if err != nil {
-      fmt.Printf("error: could not read file %s", journalFile)
-      os.Exit(2)
-    }
+	file, err := os.OpenFile(journalFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		fmt.Printf("error: could not read file %s\n", journalFile)
+		os.Exit(2)
+	}
 
-    datestamp := time.Now().Format("2006-01-02 15:04")
+	datestamp := time.Now().Format("2006-01-02 15:04")
 
-    _, err = file.Write([]byte(datestamp + " " + post + "\n"))
-    if err != nil {
-      fmt.Printf("error: could not write to file %s", journalFile)
-      os.Exit(3)
-    }
+	_, err = file.Write([]byte(datestamp + " " + post + "\n"))
+	if err != nil {
+		fmt.Printf("error: could not write to file %s\n", journalFile)
+		os.Exit(3)
+	}
 
-    file.Close()
+	file.Close()
 }
-
